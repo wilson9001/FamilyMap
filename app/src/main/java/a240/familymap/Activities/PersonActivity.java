170715801fab910ac77e45dfb77ed123bef96923
Alexander Wilson <wilsonalexanderjohn@gmail.com>
2017-12-12 20:13:55 -0700

    Got both recyclerViews working for person activity.

--- app/src/main/java/a240/familymap/Activities/PersonActivity.java
+++ app/src/main/java/a240/familymap/Activities/PersonActivity.java
@@ -22,8 +22,10 @@ import a240.familymap.R;
 
 public class PersonActivity extends AppCompatActivity
 {
-    private RecyclerView lifeEventsView;
+    private RecyclerView lifeEventsView, relativesView;
     private eventAdapter mEventAdapter;
+    private relativeAdapter mRelativeAdapter;
+    private TextView expandableEventsText, expandableRelativesText;
     private String personID;
     //private ArrayList<EventModel> Events;
 
@@ -39,7 +41,52 @@ public class PersonActivity extends AppCompatActivity
 
         personID = getIntent().getStringExtra(getString(R.string.personIDForIntent));
 
+        relativesView = findViewById(R.id.relativesList);
+
+        relativesView.setLayoutManager(new LinearLayoutManager(this));
+
         updateUI();
+
+        //expandableEventsText = findViewById(R.id.expand_events_header);
+
+       //expandableRelativesText = findViewById(R.id.expand_relatives_header);
+    }
+
+    public void toggleEventsListVisibility(View v)
+    {
+        int eventsListVisibility = lifeEventsView.getVisibility();
+        TextView eventListHeader = (TextView) v /*findViewById(R.id.expand_events_header)*/;
+
+        switch (eventsListVisibility)
+        {
+            case View.GONE:
+                lifeEventsView.setVisibility(View.VISIBLE);
+                eventListHeader.setText("{fa-compress} Life Events");
+                break;
+            default:
+                lifeEventsView.setVisibility(View.GONE);
+                eventListHeader.setText("{fa-expand} Life Events");
+        }
+    }
+
+    public void toggleRelativeListVisibility(View v)
+    {
+        int relativeListVisibility = relativesView.getVisibility();
+        TextView relativeListHeader = (TextView) v/*findViewById(R.id.expand_relatives_header)*/;
+
+        switch(relativeListVisibility)
+        {
+            case View.GONE:
+                relativesView.setVisibility(View.VISIBLE);
+                relativeListHeader.setText("{fa-compress} Relatives");
+                break;
+            case View.VISIBLE:
+                relativesView.setVisibility(View.GONE);
+                relativeListHeader.setText("{fa-expand} Relatives");
+                break;
+            default:
+
+        }
     }
 
     private void updateUI()
@@ -47,21 +94,47 @@ public class PersonActivity extends AppCompatActivity
         AppData appData = AppData.getInstance();
 
         HashMap<String, ArrayList<EventModel>> personIDToFilteredEvents = appData.getPersonIDToFilteredEvents();
+        HashMap<String, PersonModel> personIdToPersonModel = appData.getPersonIDToPersonModel();
 
         ArrayList<EventModel> Events = personIDToFilteredEvents.get(personID);
+        ArrayList<PersonModel> relatives = new ArrayList<>();
+
+        String fatherID, motherID, spouseID;
+
+        PersonModel mainPerson = personIdToPersonModel.get(personID);
+
+        fatherID = mainPerson.getFather();
+        motherID = mainPerson.getMother();
+        spouseID = mainPerson.getSpouse();
+
+        if(fatherID != null)
+        {
+            PersonModel father = personIdToPersonModel.get(fatherID);
+            relatives.add(father);
+        }
+        if(motherID != null)
+        {
+            PersonModel mother = personIdToPersonModel.get(motherID);
+            relatives.add(mother);
+        }
+        if(spouseID != null)
+        {
+            PersonModel spouse = personIdToPersonModel.get(spouseID);
+            relatives.add(spouse);
+        }
 
         if(Events == null)
         {
             Events = new ArrayList<>();
         }
 
-        HashMap<String, PersonModel> personIDToPersonModel = appData.getPersonIDToPersonModel();
+        //HashMap<String, PersonModel> personIdToPersonModel = appData.getPersonIDToPersonModel();
 
         TextView firstNameView = /*lifeEventsView.*/findViewById(R.id.personFirstName);
         TextView lastNameView = /*lifeEventsView.*/findViewById(R.id.personLaststName);
         TextView gender = /*lifeEventsView.*/findViewById(R.id.personGender);
 
-        PersonModel mainPerson = personIDToPersonModel.get(personID);
+        //PersonModel mainPerson = personIdToPersonModel.get(personID);
 
         firstNameView.setText(mainPerson.getFirstName());
         lastNameView.setText(mainPerson.getLastName());
@@ -78,6 +151,10 @@ public class PersonActivity extends AppCompatActivity
         mEventAdapter = new eventAdapter(Events);
 
         lifeEventsView.setAdapter(mEventAdapter);
+
+        mRelativeAdapter = new relativeAdapter(relatives);
+
+        relativesView.setAdapter(mRelativeAdapter);
     }
 
     private class eventHolder extends RecyclerView.ViewHolder implements View.OnClickListener
@@ -212,6 +289,153 @@ public class PersonActivity extends AppCompatActivity
         public int getItemCount()
         {
             return events.size();
+        }
+    }
+
+    private class relativeHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    {
+        private TextView relativeName, relativeRelationShip, genderIcon;
+
+        private PersonModel relativeModel, mainPerson;
+
+        public relativeHolder(LayoutInflater layoutInflater, ViewGroup parent)
+        {
+            super(layoutInflater.inflate(R.layout.list_item_person, parent, false));
+
+            itemView.setOnClickListener(this);
+
+            relativeName = itemView.findViewById(R.id.relativeNameText);
+            relativeRelationShip = itemView.findViewById(R.id.relativeRelationShip);
+            genderIcon = itemView.findViewById(R.id.relativeGenderIcon);
+
+            AppData appData = AppData.getInstance();
+            HashMap<String, PersonModel> personIdToPersonModel = appData.getPersonIDToPersonModel();
+
+            mainPerson = personIdToPersonModel.get(personID);
+        }
+
+        @Override
+        public void onClick(View view)
+        {
+            Toast.makeText(getBaseContext(), relativeModel.getPersonID(), Toast.LENGTH_SHORT).show();
+        }
+
+        public void bind(PersonModel personModel)
+        {
+            relativeModel = personModel;
+
+            StringBuilder eventInfoBuilder = new StringBuilder();
+
+            eventInfoBuilder.append(personModel.getFirstName());
+            eventInfoBuilder.append(" ");
+            eventInfoBuilder.append(personModel.getLastName());
+
+            relativeName.setText(eventInfoBuilder.toString());
+
+            eventInfoBuilder.setLength(0);
+
+            //PersonModel mainPerson = personIdToPersonModel.get(eventModel.getPerson());
+
+            String fatherID, motherID, spouseID;
+
+            fatherID = mainPerson.getFather();
+            motherID = mainPerson.getMother();
+            spouseID = mainPerson.getSpouse();
+
+            if(fatherID != null && fatherID.equals(personModel.getPersonID()))
+            {
+                relativeRelationShip.setText("Father");
+                genderIcon.setText("{fa-male 25dp}");
+            }
+            else if(motherID != null && motherID.equals(personModel.getPersonID()))
+            {
+                relativeRelationShip.setText("Mother");
+                genderIcon.setText("{fa-female 25dp}");
+            }
+            else
+            {
+                relativeRelationShip.setText("Spouse");
+                String spouseGenderIcon = personModel.getGender().equals("m") ? "{fa-male 25dp}" : "{fa-female 25dp}";
+                genderIcon.setText(spouseGenderIcon);
+            }
+
+        }
+    }
+
+    private class relativeAdapter extends RecyclerView.Adapter<relativeHolder>
+    {
+        private List<PersonModel> people;
+
+        public relativeAdapter(List<PersonModel> people)
+        {
+            this.people = people;
+        }
+
+        /**
+         * Called when RecyclerView needs a new {link ViewHolder} of the given type to represent
+         * an item.
+         * <p>
+         * This new ViewHolder should be constructed with a new View that can represent the items
+         * of the given type. You can either create a new View manually or inflate it from an XML
+         * layout file.
+         * <p>
+         * The new ViewHolder will be used to display items of the adapter using
+         * {link #onBindViewHolder(ViewHolder, int, List)}. Since it will be re-used to display
+         * different items in the data set, it is a good idea to cache references to sub views of
+         * the View to avoid unnecessary {@link View#findViewById(int)} calls.
+         *
+         * @param parent   The ViewGroup into which the new View will be added after it is bound to
+         *                 an adapter position.
+         * @param viewType The view type of the new View.
+         * @return A new ViewHolder that holds a View of the given view type.
+         * @see #getItemViewType(int)
+         * see #onBindViewHolder(ViewHolder, int)
+         */
+        @Override
+        public relativeHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            LayoutInflater layoutInflater = LayoutInflater.from(getBaseContext());
+
+            return new relativeHolder(layoutInflater, parent);
+        }
+
+        /**
+         * Called by RecyclerView to display the data at the specified position. This method should
+         * update the contents of the {link ViewHolder#itemView} to reflect the item at the given
+         * position.
+         * <p>
+         * Note that unlike {@link ListView}, RecyclerView will not call this method
+         * again if the position of the item changes in the data set unless the item itself is
+         * invalidated or the new position cannot be determined. For this reason, you should only
+         * use the <code>position</code> parameter while acquiring the related data item inside
+         * this method and should not keep a copy of it. If you need the position of an item later
+         * on (e.g. in a click listener), use {link ViewHolder#getAdapterPosition()} which will
+         * have the updated adapter position.
+         * <p>
+         * Override {link #onBindViewHolder(ViewHolder, int, List)} instead if Adapter can
+         * handle efficient partial bind.
+         *
+         * @param holder   The ViewHolder which should be updated to represent the contents of the
+         *                 item at the given position in the data set.
+         * @param position The position of the item within the adapter's data set.
+         */
+        @Override
+        public void onBindViewHolder(relativeHolder holder, int position)
+        {
+            PersonModel person = people.get(position);
+
+            holder.bind(person);
+        }
+
+        /**
+         * Returns the total number of items in the data set held by the adapter.
+         *
+         * @return The total number of items in this adapter.
+         */
+        @Override
+        public int getItemCount()
+        {
+            return people.size();
         }
     }
 }
